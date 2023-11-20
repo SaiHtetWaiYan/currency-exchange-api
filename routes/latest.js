68c318ef9c9e23dd680faaea5f4c1ed82b454dab
Sai Htet Wai Yan <saihtetwaiyan18@gmail.com>
2023-11-20 16:58:03 +0630

    fix latest

--- routes/latest.js
+++ routes/latest.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-router.use(express.json());
-router.get("/", async (req, res, next) => {
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
+const fs = require("fs").promises;
 
+router.use(express.json());
+
+router.get("/", async (req, res) => {
+  try {
+    const data = await fs.readFile("data.json", "utf8");
     const jsonData = JSON.parse(data);
     res.json(jsonData);
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
