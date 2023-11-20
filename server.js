35dc5f413a14779dafcb60fbc205c473ea0db6cf
Sai Htet Wai Yan <saihtetwaiyan18@gmail.com>
2023-11-20 15:22:46 +0630

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
