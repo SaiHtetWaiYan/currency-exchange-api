88439463d536556bd64898fd892ecc21772088e3
Sai Htet Wai Yan <saihtetwaiyan18@gmail.com>
2023-11-20 16:55:43 +0630

    update data

--- routes/latest.js
+++ routes/latest.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const fs = require("fs");
+router.use(express.json());
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
+  fs.readFile("data.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
   });
 });
 
