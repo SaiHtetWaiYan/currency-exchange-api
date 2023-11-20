552126f0435cac06b3480a323c1b3b86507efde6
Sai Htet Wai Yan <saihtetwaiyan18@gmail.com>
2023-11-20 19:40:50 +0630

    update latest.js

--- routes/latest.js
+++ routes/latest.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs").promises;
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    info: "Market Rate",
-    description: "Market rates for various currencies",
-    rates: {
-      USD: "3,400.0",
-      EUR: "3,400.0",
-    },
-  });
+  const path = require("path");
+  const filePath = path.join(__dirname, "data.json");
+
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    const jsonData = JSON.parse(data);
+
+    res.json(jsonData);
+  } catch (err) {
+    console.error("Error reading file:", err);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
